a3c55a471c8c8accd964dc6ad0c96a877a9fab9f
Felipe Marques <felipemarques@bb.com.br>
2025-06-27 11:54:16 -0300

    Excluindo alguns comentários

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-//let titulo = document.querySelector("h1");
-//titulo.innerHTML = "Jogo do Número Secreto";
-
-//let paragrafo = document.querySelector("p");
-//paragrafo.innerHTML = "Escolha um número entre 1 e 10";
 
 let listaNumerosSorteados = [];
 let numeroLimite = 10;
